4392cec9589461020cb5cc01f24346a06d65bcd6
ISON <azzgo@users.noreply.github.com>
2023-10-13 10:09:00 +0800

    feat: impl login failed with username and password invalid

--- web/test/pact-comsumer/login.test.ts
+++ web/test/pact-comsumer/login.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "vitest";
+import { beforeEach, describe, expect, test } from "vitest";
 import { renderHook } from "@testing-library/react";
 import { provider } from "./pact.utils";
 import { configEnv } from "../../src/utils/env";
@@ -6,8 +6,14 @@ import { useUserController, userAtom } from "../../src/model/user";
 import { initAxiosInstance } from "../../src/utils/request";
 import { getDefaultStore } from "jotai";
 import { getToken } from "../../src/utils/token";
+import exp from "constants";
+import { AxiosError } from "axios";
 
 describe("comsumer test for login", () => {
+  beforeEach(() => {
+    localStorage.clear();
+    getDefaultStore().set(userAtom, null);
+  });
   test("when login succuess should update store and token", async () => {
     provider
       .given("user exists")
@@ -42,7 +48,7 @@ describe("comsumer test for login", () => {
     return provider.executeTest(async (mockServer) => {
       configEnv({ BASE_URL: mockServer.url });
       initAxiosInstance();
-      const {  result } = renderHook(() => useUserController());
+      const { result } = renderHook(() => useUserController());
       await result.current.login("jake@jake.jake", "jakejake");
       expect(getDefaultStore().get(userAtom)).toEqual({
         email: "jake@jake.jake",
@@ -51,6 +57,51 @@ describe("comsumer test for login", () => {
         image: "http://image.url",
       });
       expect(getToken()).toEqual("jwt.token.here");
+    });
+  });
+
+  test("when login failed should not update store and token and throw error", async () => {
+    provider
+      .given("user exists")
+      .uponReceiving("a request to login")
+      .withRequest({
+        method: "POST",
+        path: "/login",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {
+          user: {
+            email: "fake@jake.jake",
+            password: "fakefake",
+          },
+        },
+      })
+      .willRespondWith({
+        status: 403,
+        body: {
+          errors: {
+            "email or password": ["is invalid"],
+          },
+        },
+      });
+    return provider.executeTest(async (mockServer) => {
+      configEnv({ BASE_URL: mockServer.url });
+      initAxiosInstance();
+      const { result } = renderHook(() => useUserController());
+
+      const capturedError: AxiosError = await result.current
+        .login("fake@jake.jake", "fakefake")
+        .catch((e) => e);
+      expect(capturedError.response?.status).toEqual(403);
+      expect(capturedError.response?.data).toEqual({
+        errors: {
+          "email or password": ["is invalid"],
+        },
+      });
+
+      expect(getDefaultStore().get(userAtom)).toEqual(null);
+      expect(getToken()).toEqual(null);
     });
   });
 });
