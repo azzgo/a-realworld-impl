edc63a59b2fb11d3801086afcfdcaecaf0c12900
ISON <azzgo@users.noreply.github.com>
2023-10-22 11:47:28 +0800

    [feat & refactor] to support query by author & tagName
    with Condition:
    - author and tagName both exist
    - only one of the author and tagName
    - non of them provide

--- server/src/main/kotlin/how/realworld/server/repository/ArticleRepository.kt
+++ server/src/main/kotlin/how/realworld/server/repository/ArticleRepository.kt
@@ -2,16 +2,33 @@ package how.realworld.server.repository
 
 import how.realworld.server.model.ArticleId
 import how.realworld.server.repository.mapper.ArticleMapper
+import how.realworld.server.repository.mapper.TagMapper
+import jakarta.persistence.criteria.Predicate
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.domain.Specification
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ArticleRepository: JpaRepository<ArticleMapper, ArticleId> {
-    @Query("SELECT a FROM ArticleMapper a JOIN a.tagList t WHERE t.name = :tagName")
-    fun findByTag(tagName: String, pageable: Pageable): Page<ArticleMapper>
+interface ArticleRepository: JpaRepository<ArticleMapper, ArticleId>, JpaSpecificationExecutor<ArticleMapper>
 
-    fun findByAuthorId(authorId: String, pageable: Pageable): Page<ArticleMapper>
+fun ArticleRepository.queryList(tagName: String? = null, authorId: String? = null, pageable: Pageable): Page<ArticleMapper> {
+    val specification = Specification<ArticleMapper> { root, _, criteriaBuilder ->
+        val predicates = mutableListOf<Predicate>()
+
+        if (tagName != null) {
+            val join = root.join<ArticleMapper, TagMapper>("tagList")
+            predicates.add(criteriaBuilder.equal(join.get<String>("name"), tagName))
+        }
+
+        if (authorId != null) {
+            predicates.add(criteriaBuilder.equal(root.get<String>("authorId"), authorId))
+        }
+
+        criteriaBuilder.and(*predicates.toTypedArray())
+    }
+
+    return findAll(specification, pageable)
 }
--- server/src/test/kotlin/how/realworld/server/repository/ArticleRepositoryTest.kt
+++ server/src/test/kotlin/how/realworld/server/repository/ArticleRepositoryTest.kt
@@ -94,7 +94,7 @@ class ArticleRepositoryTest {
             )
         )
 
-        val page = articleRepository.findAll(PageRequest.of(0, 3, Sort.by("createdAt").descending()))
+        val page = articleRepository.queryList(null, null, PageRequest.of(0, 3, Sort.by("createdAt").descending()))
 
         assertThat(page.totalElements, equalTo(4))
         assertThat(
@@ -150,7 +150,10 @@ class ArticleRepositoryTest {
             )
         )
 
-        val page1 = articleRepository.findByTag("tag1", PageRequest.of(0, 3, Sort.by("createdAt").descending()))
+        val page1 = articleRepository.queryList(
+            tagName = "tag1",
+            pageable = PageRequest.of(0, 3, Sort.by("createdAt").descending())
+        )
 
         assertThat(page1.totalElements, equalTo(2))
         assertThat(
@@ -162,7 +165,10 @@ class ArticleRepositoryTest {
             )
         )
 
-        val page2 = articleRepository.findByTag("tag2", PageRequest.of(0, 3, Sort.by("createdAt").descending()))
+        val page2 = articleRepository.queryList(
+            tagName = "tag2",
+            pageable = PageRequest.of(0, 3, Sort.by("createdAt").descending())
+        )
         assertThat(page2.totalElements, equalTo(4))
         assertThat(
             page2.toList().map { it.id }, equalTo(
@@ -215,13 +221,87 @@ class ArticleRepositoryTest {
             )
         )
 
-        val page1 = articleRepository.findByAuthorId("user_id_1", PageRequest.of(0, 3, Sort.by("createdAt").descending()))
+        val page1 =
+            articleRepository.queryList(authorId = "user_id_1", pageable =PageRequest.of(0, 3, Sort.by("createdAt").descending()))
 
         assertThat(page1.totalElements, equalTo(2))
         assertThat(
             page1.toList().map { it.id }, equalTo(
                 listOf(
                     savedArticleList[1].id,
+                    savedArticleList[0].id,
+                )
+            )
+        )
+    }
+
+    @Test
+    @Transactional
+    fun should_query_filtered_tag_and_authorId_articles() {
+        val tags1 = tagRepository.saveAll(listOf(TagMapper(name = "tag1"), TagMapper(name = "tag2")))
+        val tags2 = listOf(tags1.last(), tagRepository.save(TagMapper(name = "tag3")))
+        val savedArticleList = articleRepository.saveAll(
+            listOf(
+                ArticleMapper(
+                    title = "title1",
+                    description = "description1",
+                    body = "body1",
+                    tagList = tags1,
+                    authorId = "user_id_1",
+                    createdAt = Instant.parse("2016-02-18T03:22:56.637Z"),
+                ),
+                ArticleMapper(
+                    title = "title2",
+                    description = "description2",
+                    body = "body2",
+                    tagList = tags1,
+                    authorId = "user_id_2",
+                    createdAt = Instant.parse("2017-02-18T03:22:56.637Z"),
+                ),
+                ArticleMapper(
+                    title = "title3",
+                    description = "description3",
+                    body = "body3",
+                    tagList = tags2,
+                    authorId = "user_id_1",
+                    createdAt = Instant.parse("2018-02-18T03:22:56.637Z"),
+                ),
+                ArticleMapper(
+                    title = "title4",
+                    description = "description4",
+                    body = "body4",
+                    tagList = tags2,
+                    authorId = "user_id_2",
+                    createdAt = Instant.parse("2019-02-18T03:22:56.637Z"),
+                )
+            )
+        )
+
+        val page1 = articleRepository.queryList(
+            "tag1",
+            "user_id_1",
+            PageRequest.of(0, 3, Sort.by("createdAt").descending())
+        )
+
+        assertThat(page1.totalElements, equalTo(1))
+        assertThat(
+            page1.toList().map { it.id }, equalTo(
+                listOf(
+                    savedArticleList[0].id,
+                )
+            )
+        )
+
+        val page2 = articleRepository.queryList(
+            "tag2",
+            "user_id_1",
+            PageRequest.of(0, 3, Sort.by("createdAt").descending())
+        )
+        assertThat(page2.totalElements, equalTo(2))
+        assertThat(
+            page2.toList().map { it.id }, equalTo(
+                listOf(
+                    savedArticleList[2].id,
                     savedArticleList[0].id,
                 )
             )
