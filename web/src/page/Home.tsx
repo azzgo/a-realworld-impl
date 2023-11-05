2eb17da59db2bf76ce3cdb4d865466b1964c6828
ISON <azzgo@users.noreply.github.com>
2023-11-05 18:36:59 +0800

    feat: extract banner

--- web/src/page/Home.tsx
+++ web/src/page/Home.tsx
@@ -1,15 +1,10 @@
 import "./Home.css";
+import Banner from "../components/Banner";
 
 function Home() {
   return (
     <div className="home-page">
-      <div className="banner">
-        <div className="container">
-          <h1 className="logo-font">conduit</h1>
-          <p>A place to share your knowledge.</p>
-        </div>
-      </div>
-
+      <Banner />
       <div className="container page">
         <div className="row">
           <div className="col-md-9">
